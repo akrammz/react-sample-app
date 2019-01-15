db97c67542f3ce4b8d71886cc741fd734ade104e
Akram Mohammed <akram.mohammed@forcare.com>
2019-01-15 13:56:51 -0500

    Change the App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1> Andre Tan is awesome Web Developer </h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            
           </p>
           <a
             className="App-link"
